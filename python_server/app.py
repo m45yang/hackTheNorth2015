e93def8ce9b8cf25d7f62fee0bcd0c1515eb3e8c
m45yang <m45yang@uwaterloo.ca>
2015-09-19 15:05:00 -0400

    lel

--- python_server/app.py
+++ python_server/app.py
@@ -8,20 +8,35 @@ import praw
 app = Flask(__name__)
  
 indicoio.config.api_key = 'e77398fb1e34de03ac0b22d09d5fd21a'
-SEARCH_LIMIT = 100
+COMMENT_SEARCH_LIMIT = 5
+SUBMISSION_SEARCH_LIMIT = 10
 
 @app.route("/", methods=['GET'])
 def home():
-	# print(indicoio.sentiment('indico is so easy to use!'))
 	return render_template('home.html')
 
 @app.route("/search", methods=['POST'])
 def search():
 	searchName = request.form['searchName']
 	r = praw.Reddit(user_agent='sentimenter')
-	submissions = r.get_subreddit(searchName).get_comments(limit=SEARCH_LIMIT)
-	print(indicoio.sentiment([str(x) for x in submissions]))
-	return render_template('search.html', searchName = searchName)
+	subReddits = r.search(searchName)
+	score = 0
+	searchCommentInd = 0
+	searchSubmissionInd = 0
+	for s in subReddits:
+		comments = s.comments
+		for c in comments:
+			score += indicoio.sentiment(str(c))
+			searchCommentInd += 1
+			if (searchCommentInd == COMMENT_SEARCH_LIMIT):
+				searchCommentInd = 0
+				break
+			print(str(c))
+		searchSubmissionInd += 1
+		if (searchSubmissionInd == SUBMISSION_SEARCH_LIMIT):
+			break
+	score = score / (COMMENT_SEARCH_LIMIT * SUBMISSION_SEARCH_LIMIT)
+	return render_template('search.html', searchName = searchName, score = score, s = str(c))
 
 
 if __name__ == "__main__":
