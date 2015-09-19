fe971f8112ee4de5f30e34113e62c271f1a0d67e
m45yang <m45yang@uwaterloo.ca>
2015-09-19 05:52:59 -0400

    added folder structure for python server

---
+++ python_server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+ 
+app = Flask(__name__)
+ 
+@app.route("/", methods=['GET'])
+def home():
+	return render_template('home.html')
+
+ 
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
